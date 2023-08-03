389cd1cf77b1a7bce44a085b735a4d39d0ab7dd5
jerenbashimova <129326466+jerenbashimova@users.noreply.github.com>
2023-08-03 13:50:48 +0300

    Initial Commit

---
+++ latest_news_app/latest_news_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  latest_news_appApp.swift
+//  latest_news_app
+//
+//  Created by j.bashimova on 3.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct latest_news_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
